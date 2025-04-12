436b5097fc93be2e56082bd9f0d42ff9504e9952
gi199 <looksmc@outlook.com>
2025-04-12 22:31:57 +0800

    Edit

--- src/main/java/com/gi199/randomod/ItemClasses.java
+++ src/main/java/com/gi199/randomod/ItemClasses.java
@@ -1,21 +1,20 @@
 package com.gi199.randomod;
 
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.component.type.ConsumableComponent;
 import net.minecraft.component.type.ConsumableComponents;
 import net.minecraft.component.type.FoodComponent;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.SwordItem;
-import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.*;
 import net.minecraft.item.consume.ApplyEffectsConsumeEffect;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 import java.util.function.Function;
@@ -44,6 +43,14 @@ public class ItemClasses {
                 .register((itemGroup) -> itemGroup.add(ItemClasses.SECRET_FOOD));
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
                 .register((itemGroup) -> itemGroup.add(ItemClasses.SECRET_SWORD));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
+                .register((itemGroup) -> itemGroup.add(SecretArmorMaterial.SECRET_BOOTS));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
+                .register((itemGroup) -> itemGroup.add(SecretArmorMaterial.SECRET_LEGGINGS));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
+                .register((itemGroup) -> itemGroup.add(SecretArmorMaterial.SECRET_CHESTPLATE));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
+                .register((itemGroup) -> itemGroup.add(SecretArmorMaterial.SECRET_HELMET));
 
     }
     public static final ConsumableComponent POISON_FOOD_CONSUMABLE_COMPONENT = ConsumableComponents.food()
@@ -63,6 +70,26 @@ public class ItemClasses {
             null
     );
     public static final Item SECRET_SWORD = register("secret_sword", settings -> new SwordItem(SecretToolMaterial, 1000000000000f, 100000000000000f, settings), new Item.Settings());
+    public static final RegistryKey<ItemGroup> CUSTOM_ITEM_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(FabricDocsReference.MOD_ID, "item_group"));
+    public static final ItemGroup CUSTOM_ITEM_GROUP = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(ItemClasses.SECRET_ITEM))
+            .displayName(Text.translatable("itemGroup.randomod"))
+            .build();
+    // Register the group.
+    Registry.Register(Registries.ITEM_GROUP, CUSTOM_ITEM_GROUP_KEY, CUSTOM_ITEM_GROUP);
+
+    // Register items to the custom item group.
+    ItemGroupEvents.modifyEntriesEvent(CUSTOM_ITEM_GROUP_KEY).register(itemGroup -> {
+        itemGroup.add(ModItems.SUSPICIOUS_SUBSTANCE);
+        itemGroup.add(ModItems.POISONOUS_APPLE);
+        itemGroup.add(ModItems.GUIDITE_SWORD);
+        itemGroup.add(ModItems.GUIDITE_HELMET);
+        itemGroup.add(ModItems.GUIDITE_BOOTS);
+        itemGroup.add(ModItems.GUIDITE_LEGGINGS);
+        itemGroup.add(ModItems.GUIDITE_CHESTPLATE);
+        itemGroup.add(ModItems.LIGHTNING_STICK);
+        // ...
+    };
 
 }
 
--- src/main/java/com/gi199/randomod/RandoMod.java
+++ src/main/java/com/gi199/randomod/RandoMod.java
@@ -1,7 +1,6 @@
 package com.gi199.randomod;
 
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
