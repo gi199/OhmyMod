ffde639d96757dee4985982193ad400f8bbb4530
gi199 <looksmc@outlook.com>
2025-04-13 16:59:49 +0800

    Initial commit

---
+++ src/main/java/com/gi199/randomod/ItemClasses.java
@@ -0,0 +1,94 @@
+package com.gi199.randomod;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.networking.v1.S2CConfigurationChannelEvents;
+import net.minecraft.component.type.ConsumableComponent;
+import net.minecraft.component.type.ConsumableComponents;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.*;
+import net.minecraft.item.consume.ApplyEffectsConsumeEffect;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class ItemClasses {
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        // Create the item key.
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(RandoMod.MOD_ID, name));
+
+        // Create the item instance.
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        // Register the item.
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
+    }
+    public static final Item SECRET_ITEM = register("secret_item", Item::new, new Item.Settings());
+
+    public static void initialize() {
+        // Get the event for modifying entries in the ingredients group.
+// And register an event handler that adds our suspicious item to the ingredients group.
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(ItemClasses.SECRET_ITEM));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK)
+                .register((itemGroup) -> itemGroup.add(ItemClasses.SECRET_FOOD));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
+                .register((itemGroup) -> itemGroup.add(ItemClasses.SECRET_SWORD));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
+                .register((itemGroup) -> itemGroup.add(SecretArmorMaterial.SECRET_BOOTS));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
+                .register((itemGroup) -> itemGroup.add(SecretArmorMaterial.SECRET_LEGGINGS));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
+                .register((itemGroup) -> itemGroup.add(SecretArmorMaterial.SECRET_CHESTPLATE));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
+                .register((itemGroup) -> itemGroup.add(SecretArmorMaterial.SECRET_HELMET));
+
+    }
+    public static final ConsumableComponent POISON_FOOD_CONSUMABLE_COMPONENT = ConsumableComponents.food()
+            // The duration is in ticks, 20 ticks = 1 second
+            .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 10000 * 20, 1), 1.0f))
+            .build();
+    public static final FoodComponent POISON_FOOD_COMPONENT = new FoodComponent.Builder()
+            .alwaysEdible()
+            .build();
+    public static final Item SECRET_FOOD = register("secret_food", Item::new, new Item.Settings().food(POISON_FOOD_COMPONENT, POISON_FOOD_CONSUMABLE_COMPONENT));
+    public static final ToolMaterial SecretToolMaterial = new ToolMaterial(
+            BlockTags.INCORRECT_FOR_WOODEN_TOOL,
+            455,
+            5.0F,
+            1.5F,
+            22,
+            null
+    );
+    public static final Item SECRET_SWORD = register("secret_sword", settings -> new SwordItem(SecretToolMaterial, 1000000000000f, 100000000000000f, settings), new Item.Settings());
+    public static final RegistryKey<ItemGroup> CUSTOM_ITEM_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(FabricDocsReference.MOD_ID, "item_group"));
+    public static final ItemGroup CUSTOM_ITEM_GROUP = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(ItemClasses.SECRET_ITEM))
+            .displayName(Text.translatable("itemGroup.randomod"))
+            .build();
+    // Register the group.
+    Registry.Register(Registries.ITEM_GROUP, CUSTOM_ITEM_GROUP_KEY, CUSTOM_ITEM_GROUP);
+    ItemGroupEvents.modifyEntriesEvent(CUSTOM_ITEM_GROUP_KEY).register(itemGroup -> {
+        itemGroup.add(ModItems.SUSPICIOUS_SUBSTANCE);
+        itemGroup.add(ModItems.POISONOUS_APPLE);
+        itemGroup.add(ModItems.GUIDITE_SWORD);
+        itemGroup.add(ModItems.GUIDITE_HELMET);
+        itemGroup.add(ModItems.GUIDITE_BOOTS);
+        itemGroup.add(ModItems.GUIDITE_LEGGINGS);
+        itemGroup.add(ModItems.GUIDITE_CHESTPLATE);
+        itemGroup.add(ModItems.LIGHTNING_STICK);
+        // ...
+    };
+
+}
+
---
+++ src/main/java/com/gi199/randomod/mixin/ExampleMixin.java
@@ -0,0 +1,20 @@
+package com.gi199.randomod.mixin;
+
+import net.minecraft.server.MinecraftServer;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+@Mixin(MinecraftServer.class)
+public class ExampleMixin {
+	@Shadow @Final private static Logger LOGGER;
+
+	@Inject(at = @At("HEAD"), method = "loadWorld")
+	private void init(CallbackInfo info) {
+		// This code is injected into the start of MinecraftServer.loadWorld()V
+		LOGGER.info("Man");
+	}
+}
